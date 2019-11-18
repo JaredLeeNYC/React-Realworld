f224bd06a1f7db4b4bea0d8cc15541fcf75dacdb
Jared Lee <lzbstar@gmail.com>
2019-11-18 20:40:13 +0800

    fix YourFeed && GlobalFeed getArticlesByTag

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,16 +9,22 @@ export default function Home() {
   const [tags, settags] = useState([]);
   let [isGlobal, setisGlobal] = useState(false);
   const [auth] = useContext(authContext);
-  const [byTags, setbyTags] = useState([]);
+  const [byTags, setbyTags] = useState(["Global Feed"]);
   const [tagOnClick, setTagOnClick] = useState("");
 
   useEffect(() => {
     agent.Articles.all().then(res => {
       // console.log(res.articles);
+      if (auth.username) {
+        setbyTags(["Your Feed", "Global Feed"]);
+        agent.Articles.feed().then(res => {
+          setarticles(res.articles);
+        });
+      }
       setarticles(res.articles);
     });
     return () => {};
-  }, []);
+  }, [auth.username]);
 
   useEffect(() => {
     agent.Tags.getAll().then(res => {
@@ -31,27 +37,29 @@ export default function Home() {
     if (!auth.username) {
       return (
         <ul className="nav nav-pills outline-active">
-          <li className="nav-item">
-            <a
+          {/* <li className="nav-item">
+            <Link
+              to=""
               className={
                 props.byTags.length === 0 ? "nav-link active" : "nav-link"
               }
               onClick={youOrGlobal}
             >
               Global Feed
-            </a>
-          </li>
+            </Link>
+          </li> */}
           {props.byTags.map(byTag => {
             return (
               <li className="nav-item" key={byTag}>
-                <a
+                <Link
+                  to=""
                   className={
                     props.tagOnClick === byTag ? "nav-link active" : "nav-link"
                   }
                   onClick={() => articlesByTag(byTag)}
                 >
                   #{byTag}
-                </a>
+                </Link>
               </li>
             );
           })}
@@ -60,37 +68,40 @@ export default function Home() {
     }
     return (
       <ul className="nav nav-pills outline-active">
-        <li className="nav-item">
-          <a
+        {/* <li className="nav-item">
+          <Link
+            to=""
             className={
               props.isGlobal && !props.byTags ? "nav-link" : "nav-link active"
             }
             onClick={youOrGlobal}
           >
             Your Feed
-          </a>
+          </Link>
         </li>
         <li className="nav-item">
-          <a
+          <Link
+            to=""
             className={
               props.isGlobal && !props.byTags ? "nav-link active" : "nav-link"
             }
             onClick={youOrGlobal}
           >
             Global Feed
-          </a>
-        </li>
+          </Link>
+        </li> */}
         {props.byTags.map(byTag => {
           return (
             <li className="nav-item" key={byTag}>
-              <a
+              <Link
+                to=""
                 className={
                   props.tagOnClick === byTag ? "nav-link active" : "nav-link"
                 }
                 onClick={() => articlesByTag(byTag)}
               >
                 #{byTag}
-              </a>
+              </Link>
             </li>
           );
         })}
@@ -98,31 +109,41 @@ export default function Home() {
     );
   };
 
-  function youOrGlobal() {
-    if (isGlobal) {
-      agent.Articles.feed().then(res => {
-        setarticles(res.articles);
-        setbyTags([]);
-      });
-    } else {
-      agent.Articles.all().then(res => {
-        setarticles(res.articles);
-        setbyTags([]);
-      });
-    }
-    setisGlobal(!isGlobal);
-  }
+  // function youOrGlobal() {
+  //   if (isGlobal) {
+  //     agent.Articles.feed().then(res => {
+  //       setarticles(res.articles);
+  //       setbyTags([]);
+  //     });
+  //   } else {
+  //     agent.Articles.all().then(res => {
+  //       setarticles(res.articles);
+  //       setbyTags([]);
+  //     });
+  //   }
+  //   setisGlobal(!isGlobal);
+  // }
 
   function articlesByTag(tag) {
     setTagOnClick(tag);
-    agent.Articles.byTag(tag, 1).then(res => {
-      let newByTags = [...byTags];
-      setarticles(res.articles);
-      if (byTags.indexOf(tag) === -1) {
-        console.log([...newByTags, tag]);
-        setbyTags([...newByTags, tag]);
-      }
-    });
+    if (tag === "Global Feed") {
+      agent.Articles.all().then(res => {
+        setarticles(res.articles);
+      });
+    } else if (tag === "Your Feed") {
+      agent.Articles.feed().then(res => {
+        setarticles(res.articles);
+      });
+    } else {
+      agent.Articles.byTag(tag, 1).then(res => {
+        let newByTags = [...byTags];
+        setarticles(res.articles);
+        if (byTags.indexOf(tag) === -1) {
+          console.log([...newByTags, tag]);
+          setbyTags([...newByTags, tag]);
+        }
+      });
+    }
   }
 
   return (
@@ -156,13 +177,14 @@ export default function Home() {
 
               <div className="tag-list">
                 {tags.map((tag, index) => (
-                  <a
+                  <Link
+                    to=""
                     className="tag-pill tag-default"
                     key={index}
                     onClick={() => articlesByTag(tag)}
                   >
                     {tag}
-                  </a>
+                  </Link>
                 ))}
               </div>
             </div>
